7c852c0cbe03f0a18e638e733cfed4c3819da049
rafae2k <53544360+rafae2k@users.noreply.github.com>
2022-01-01 14:44:41 -0300

    ci(index): index skeleton

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         'primary-yellow': 'hsl(51, 100%, 49%)',
         'graphic-design-text': 'hsl(167, 40%, 24%)',
         'photography-text': 'hsl(198, 62%, 26%)',
-        footer: 'hsl(168, 34%, 41%)',
+        'page-footer': 'hsl(168, 34%, 41%)',
       },
       fontFamily: {
         sans: 'Barlow, sans-serif',
